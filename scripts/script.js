986467ad4807372d9ce1a3e277cfd4122b4ff3a8
OptimusRhyme <cchase.junk@gmail.com>
2014-08-09 22:21:37 -0700

    Build out the basic framework to fit the rest of the content into, including sidebar nav, header, and helpbar

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+
+//Help Bar
+$(".help-bar-content").hide();
+
+$("#toggle-help").click(function() {
+
+	var helpBar = $("#help-bar");
+
+	if (helpBar.hasClass("closed")) {
+		helpBar.animate( {
+			right: "0px"
+		}, 400)
+
+		helpBar.attr("class", "open")
+	    $(".help-bar-content").show();
+		$("#toggle-help").attr("class", "fa fa-long-arrow-right")
+	}
+
+	else {
+		helpBar.animate( {
+			right: "-270px"
+		}, 400, function() {
+			$(".help-bar-content").hide();
+		})
+
+		helpBar.attr("class", "closed")
+		$("#toggle-help").attr("class", "fa fa-long-arrow-left")	
+	}
+});
